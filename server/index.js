d1532ca615ada5e31daeb241edc4caa8a8fd0dac
James Knight <contact@mtn-studio.com>
2023-02-13 14:18:43 -0800

    Changed root of repo to include server folder.
    Also started init commit for server side. Connected to MongoDB, installed deps and started on controllers + routes.

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+
+// /api/v1/properties/create
+
+dotenv.config();
+
+const app = express();
+// Add CORS as middleware
+app.use(cors());
+
+// Add Express as middleware, limit file size from frontend to 50mb
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', (req, res) => {
+	res.send({ message: 'Hello World!' });
+});
+
+const startServer = async () => {
+	try {
+		// Connect to database
+		connectDB(process.env.MONGODB_URL);
+		app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startServer();
